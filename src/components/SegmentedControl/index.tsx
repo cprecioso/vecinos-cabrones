8c073b3abacfcc4730f0045140424ea429b2f424
Carlos Precioso <kalprestito@gmail.com>
2020-04-11 20:23:35 +0200

    Make SegmentedControl functional

--- src/components/SegmentedControl/index.tsx
+++ src/components/SegmentedControl/index.tsx
@@ -1,37 +1,65 @@
-import React, { FunctionComponent } from "react"
+import clsx from "clsx"
+import React, { FunctionComponent, InputHTMLAttributes } from "react"
 import styles from "./style.module.css"
 
-type OptionProps = { name: string; checked: boolean }
-
-export type Props = {
-  options: string[]
+type OptionProps = {
+  name: string
+  checked: boolean
+  setSelected: (i: string) => void
 }
 
-const makeId = (() => {
-  let idCounter = 0
-  return () => `__option__${idCounter++}`
-})()
-
-const Option: FunctionComponent<OptionProps> = ({ name, checked }) => {
-  const id = React.useMemo(makeId, [])
+const Option: FunctionComponent<OptionProps> = ({
+  name,
+  checked,
+  setSelected,
+}) => {
+  const onChange = React.useCallback<
+    NonNullable<InputHTMLAttributes<HTMLInputElement>["onChange"]>
+  >(
+    (e) => {
+      if (e.currentTarget.checked) setSelected(name)
+    },
+    [setSelected, name]
+  )
   return (
-    <div className={styles.option}>
-      <input type="radio" id={id} checked={checked} />
-      <label htmlFor={id}>
+    <div className={clsx(styles.option, checked && styles.checked)}>
+      <label>
+        <input type="radio" checked={checked} onChange={onChange} />
         <span>{name}</span>
       </label>
     </div>
   )
 }
 
-const SegmentedControl: FunctionComponent<Props> = ({ options }) => (
-  <div className={styles.segmentedControl}>
-    <span className={styles.selection}></span>
+export type Props = {
+  options: string[]
+  selected: string
+  setSelected: (i: string) => void
+}
 
-    {options.map((option, i) => (
-      <Option key={option} name={option} checked={i === 0} />
-    ))}
-  </div>
-)
+const SegmentedControl: FunctionComponent<Props> = ({
+  options,
+  selected,
+  setSelected,
+}) => {
+  const selectedI = options.indexOf(selected)
+  return (
+    <div className={styles.segmentedControl}>
+      <span
+        className={styles.selection}
+        style={{ transform: `translateX(${selectedI * 100}%)` }}
+      />
+
+      {options.map((option, i) => (
+        <Option
+          key={option}
+          name={option}
+          checked={i === selectedI}
+          setSelected={setSelected}
+        />
+      ))}
+    </div>
+  )
+}
 
 export default SegmentedControl
