04ee1fef17a32294dd7a9fc04690ee8149280d23
Carlos Precioso <kalprestito@gmail.com>
2020-04-11 12:28:28 +0200

    Extract Error and Loading views into their own components

---
+++ src/components/FetchHelpers/index.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from "react"
+import styles from "./style.module.css"
+
+export const LoadingView: FunctionComponent = () => (
+  <h1 className={styles.title}>Buscando...</h1>
+)
+
+export const ErrorView: FunctionComponent<{ error?: any }> = ({
+  error = "",
+}) => (
+  <div>
+    <h1 className={styles.title}>Error</h1>
+    <pre className={styles.error}>{"" + (error ?? "")}</pre>
+  </div>
+)
