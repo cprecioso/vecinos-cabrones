668b3cdc23830664326bd5d8e075f2762cfc00ed
Carlos Precioso <kalprestito@gmail.com>
2020-04-11 19:26:41 +0200

    Format

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -16,24 +16,32 @@ const Main: FunctionComponent = () => (
     <SearchBar autoFocus={true} />
 
     <div className={clsx(styles.row, styles.explain)}>
-        <div className={styles["line-group"]}>
-          <hr />
-          <div className={styles["line-title"]}>
-            <div className={styles["line-title-text"]}>¿Pero esto qué es?</div>
-          </div>
+      <div className={styles["line-group"]}>
+        <hr />
+        <div className={styles["line-title"]}>
+          <div className={styles["line-title-text"]}>¿Pero esto qué es?</div>
+        </div>
       </div>
       <div className={styles["explain-image"]}>
-		  <img src="/static/emilio.jpg" />
-		  <div className={styles["explain-title"]}>Todas las escenas de ANHQV con una búsqueda</div>
-		  <div className={styles["explain-description"]}>Puedes buscar en todos los diálogos de la serie y encontrar la escena que buscas en los 90 capítulos de la serie. </div>
-	   </div>
+        <img src="/static/emilio.jpg" />
+        <div className={styles["explain-title"]}>
+          Todas las escenas de ANHQV con una búsqueda
+        </div>
+        <div className={styles["explain-description"]}>
+          Puedes buscar en todos los diálogos de la serie y encontrar la escena
+          que buscas en los 90 capítulos de la serie.{" "}
+        </div>
+      </div>
       <div className={styles["explain-image"]}>
-		  <img src="/static/movil_anhqv.jpg" />
-		  <div className={styles["explain-title"]}>Descarga un GIF de cualquier escena</div>
-		  <div className={styles["explain-description"]}>Visualiza las escenas de todos los capítulos, sus diálogos y descarga los GIFs rápidamente para compartirlos en redes sociales.</div>
-	   </div>	
-	   
-   
+        <img src="/static/movil_anhqv.jpg" />
+        <div className={styles["explain-title"]}>
+          Descarga un GIF de cualquier escena
+        </div>
+        <div className={styles["explain-description"]}>
+          Visualiza las escenas de todos los capítulos, sus diálogos y descarga
+          los GIFs rápidamente para compartirlos en redes sociales.
+        </div>
+      </div>
     </div>
   </div>
 )
