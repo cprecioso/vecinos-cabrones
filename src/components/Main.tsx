e96bd0219ebacf869e2b77c3dba570a545194eb0
Pablo Garcia Nieto Rodríguez <pablo@MacBook-Pro-de-Pablo-3.local>
2020-04-11 19:26:38 +0200

    Change on text and styles

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -32,9 +32,31 @@ const Main: FunctionComponent = () => (
 		  <div className={styles["explain-title"]}>Descarga un GIF de cualquier escena</div>
 		  <div className={styles["explain-description"]}>Visualiza las escenas de todos los capítulos, sus diálogos y descarga los GIFs rápidamente para compartirlos en redes sociales.</div>
 	   </div>	
-	   
-   
     </div>
+
+    <div className={clsx(styles.row, styles.explain)}>
+        <div className={styles["line-group"]}>
+          <hr />
+          <div className={styles["line-title"]}>
+            <div className={styles["line-title-text"]}>¿Cómo y quién ha hecho esto?</div>
+          </div>
+      </div>
+      <div className={styles["explain-image"]}>
+		  <div style={{ marginTop: 40 }} className={styles["explain-title"]}>¡Subtítulos!</div>
+		  <div className={styles["explain-description"]}>Gracias a los subtítulos de la serie, se ha vinculado cada frase a los fotogramas correspondientes y se ha generado una base de datos en la que consultar y obtener la información rápidamente.<br/> Puede leer más acerca de la tecnología usada <a href="medium.com/">aquí.</a> </div>
+	   </div>
+      <div className={styles["explain-image"]}>
+		  <div className={styles["explain-title"]}>Los creadores</div>
+		  <div className={styles["explain-description"]}>Las personas que han hecho esta web posible son:
+		  <br /><br />
+		  @pabgn: idea original y backend<br />
+		  @adrimartin: diseño UI/UX<br />
+		  @cprecioso: frontend<br />
+		  
+		  </div>
+	   </div>	
+    </div>
+        
   </div>
 )
 
