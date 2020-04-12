5dc09dc4a7861aaeac90e632a80119bee6fd741e
Carlos Precioso <kalprestito@gmail.com>
2020-04-12 11:30:01 +0200

    Refactor SubtitleView

--- src/components/Scene/SubtitleView.tsx
+++ src/components/Scene/SubtitleView.tsx
@@ -1,56 +1,41 @@
-import { FunctionComponent } from "react"
+import React, { FunctionComponent } from "react"
+import { responseInterface } from "swr"
 import LinkToSubtitle from "../LinkToSubtitle"
-import { useFetchSubtitleInContext } from "./subtitle-fetch"
+import { CacheEntry, useFetchSubtitleInContext } from "./subtitle-fetch"
 import SubtitleLine from "./SubtitleLine"
+
+const SubtitleLineWrapper: FunctionComponent<{
+  response: responseInterface<CacheEntry, any>
+  current?: boolean
+}> = ({ response, current }) => {
+  return (
+    <LinkToSubtitle result={response.data?.scene} shallow={true} scroll={false}>
+      <a>
+        <SubtitleLine
+          isCurrent={current}
+          text={
+            response.error ? "⚠️" + response.error : response.data?.scene.text
+          }
+        />
+      </a>
+    </LinkToSubtitle>
+  )
+}
 
 const SubtitleView: FunctionComponent<{
   id: number
-  onGoNext: () => void
-  onGoPrevious: () => void
-}> = ({ id, onGoNext, onGoPrevious }) => {
+}> = ({ id }) => {
   const { current, previous, next } = useFetchSubtitleInContext(id)
 
   return (
     <>
-      {[
-        current.data?.noPrevious ? null : (
-          <LinkToSubtitle
-            result={previous.data?.scene}
-            shallow={true}
-            scroll={false}
-          >
-            <a>
-              <SubtitleLine
-                key={id - 1}
-                text={
-                  previous.error
-                    ? "⚠️" + previous.error
-                    : previous.data?.scene.text
-                }
-              />
-            </a>
-          </LinkToSubtitle>
-        ),
-        <SubtitleLine
-          key={id}
-          text={current.error ? "⚠️" + current.error : current.data?.scene.text}
-          isCurrent={true}
-        />,
-        current.data?.noNext ? null : (
-          <LinkToSubtitle
-            result={next.data?.scene}
-            shallow={true}
-            scroll={false}
-          >
-            <a>
-              <SubtitleLine
-                key={id + 1}
-                text={next.error ? "⚠️" + next.error : next.data?.scene.text}
-              />
-            </a>
-          </LinkToSubtitle>
-        ),
-      ]}
+      {current.data?.noPrevious ? null : (
+        <SubtitleLineWrapper key={id - 1} response={previous} />
+      )}
+      <SubtitleLineWrapper current key={id} response={current} />
+      {current.data?.noNext ? null : (
+        <SubtitleLineWrapper key={id + 1} response={next} />
+      )}
     </>
   )
 }
