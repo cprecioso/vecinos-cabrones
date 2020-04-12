b64597cf4af9569cd49bb9552f2244e2650aabb4
Carlos Precioso <kalprestito@gmail.com>
2020-04-12 15:47:32 +0200

    Add favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,42 @@ const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Vecinos Cabrones</title>
+
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="194x194"
+        href="/favicon-194x194.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/android-chrome-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#fec32a" />
+      <meta name="apple-mobile-web-app-title" content="Vecinos Cabrones" />
+      <meta name="application-name" content="Vecinos Cabrones" />
+      <meta name="msapplication-TileColor" content="#16181b" />
+      <meta name="theme-color" content="#16181b" />
     </Head>
     <div className={styles.main}>
       <div className={styles["home-content"]}>
