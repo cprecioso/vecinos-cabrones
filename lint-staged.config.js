a3d5011caebe28d2cca3806e040bbaa8bfd494ee
Carlos Precioso <github@precioso.design>
2025-03-30 12:40:50 +0200

    chore: remove custom postcss and stylelint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,7 +4,6 @@ const prettier = "prettier --ignore-unknown --write";
 
 module.exports = {
   "package.json": [() => "yarn install --mode=update-lockfile", prettier],
-  "*.css": ["stylelint --fix", prettier],
   "yarn.lock": () => "yarn dedupe --mode=update-lockfile",
   "*.{j,t}s{,x}": ["eslint --fix", prettier],
   "*": prettier,
