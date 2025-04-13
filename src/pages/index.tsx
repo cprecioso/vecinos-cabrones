babdfdd544aa3f16e5e8c67667c648afdbb8b948
Pablo G. <pablo@garcianieto.me>
2025-04-13 13:06:01 +0200

    Donation Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import Main from "@/components/Main";
+import DonationPage from "@/components/DonationPage";
 import { NextPage } from "next";
 
-const IndexPage: NextPage = () => <Main />;
+const IndexPage: NextPage = () => <DonationPage />;
 
 export default IndexPage;
